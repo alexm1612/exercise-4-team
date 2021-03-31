70ef3d757aa38939dc2fbb9ed0b2c37bd383d787
Alex Martin <72417540+alexm1612@users.noreply.github.com>
2021-03-30 19:57:50 -0700

    added Form to index.js

--- exercise4/pages/index.js
+++ exercise4/pages/index.js
@@ -2,13 +2,14 @@ import styles from '../styles/Home.module.css';
 
 import ChatUI from '../comps/Chat';
 import PhotoUI from '../comps/photo/index';
+import FormUI from '../comps/Form';
 
 export default function Home() {
   return (
     <div id="main" className={styles.container}>
       <ChatUI />
       <PhotoUI />
-
+      <FormUI />
     </div>
   )
 }
